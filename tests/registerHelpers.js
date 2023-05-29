b0a43e74e3b3f0834ff6cd085ae468b0ab782ec2
HasanAshab <samertask@gmail.com>
2023-05-29 06:37:25 +0600

    backup

--- tests/registerHelpers.js
+++ tests/registerHelpers.js
@@ -3,26 +3,33 @@ const mongoose = require("mongoose");
 const path = require("path");
 const nodemailerMock = require("nodemailer-mock");
 
-
 global.base = (base_path = "") => {
   return path.join(path.join(__dirname, "../dist"), base_path);
-}
+};
 
 global.fakeFile = (name) => {
   return `storage/test_files/${name}`;
 };
 
 global.resetDatabase = async () => {
-  const models = mongoose.modelNames();
-  const promises = [];
-  for (const modelName of models) {
-    const Model = mongoose.model(modelName);
-    promises.push(Model.deleteMany({}));
+  try{
+    await mongoose.connection.dropDatabase();
   }
-  await Promise.all(promises);
+  catch{
+    const models = mongoose.modelNames();
+    const promises = [];
+    for (const model of models){
+      promises.push(mongoose.model(model).deleteMany({}));
+    }
+    await Promise.all(promises);
+  }
 };
 
-global.waitForEmailsSent = async function(expectedCount, timeout = 100000, interval = 500) {
+global.waitForEmailsSent = async function (
+  expectedCount,
+  timeout = 100000,
+  interval = 500
+) {
   const startTime = Date.now();
   while (Date.now() - startTime < timeout) {
     const mails = nodemailerMock.mock.getSentMail();
@@ -31,4 +38,4 @@ global.waitForEmailsSent = async function(expectedCount, timeout = 100000, inter
     }
     await new Promise((resolve) => setTimeout(resolve, interval));
   }
-}
+};
