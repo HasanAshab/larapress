0748cae05e0605dffdd90f6652e90e4cfcef39cc
HasanAshab <samertask@gmail.com>
2023-06-24 12:27:59 +0600

    backup

--- illuminate/utils/FileValidator.ts
+++ illuminate/utils/FileValidator.ts
@@ -1,4 +1,6 @@
-import { UploadedFile } from "express-fileupload";
+import {
+  UploadedFile
+} from "express-fileupload";
 
 export default class FileValidator {
   public rules: {
@@ -22,20 +24,25 @@ export default class FileValidator {
     return {
       validate(files: Record < string, UploadedFile | UploadedFile[] >): string | null {
         for (const fieldName in schema) {
-          const fileStack = Array.isArray(files[fieldName])? files[fieldName]: [files[fieldName]];
+          const fileParts = files[fieldName];
+          const fileStack = Array.isArray(fileParts)? fileParts: [fileParts];
           const {
             rules
           } = schema[fieldName];
           if (!rules.isMandatory && !fileStack[0]) continue;
           if (rules.isMandatory && !fileStack[0]) return `The ${fieldName} field is required.`;
           if (rules.parts) {
-            if (file.length !== rules.parts) return `The ${fieldName} field should have ${rules.parts} parts.`;
+            if (fileStack.length !== rules.parts) return `The ${fieldName} field should have ${rules.parts} parts.`;
           } else {
-            if (rules.maxParts && file.length > rules.maxParts) return `The ${fieldName} field should'n have more than ${rules.maxParts} parts.`;
-            if (rules.minParts && file.length < rules.minParts) return `The ${fieldName} field should have at least ${rules.minParts} parts.`;
+            if (rules.maxParts && fileStack.length > rules.maxParts) return `The ${fieldName} field should'n have more than ${rules.maxParts} parts.`;
+            if (rules.minParts && fileStack.length < rules.minParts) return `The ${fieldName} field should have at least ${rules.minParts} parts.`;
           }
 
           for (const file of fileStack) {
+            if (rules.custom) {
+              const error = rules.custom(file);
+              if (error) return error;
+            }
             if (rules.maxSize && file.size > rules.maxSize) return `The ${fieldName} field size shouldn't more than ${rules.maxSize / 1000}KB.`;
             if (rules.minSize && file.size < rules.minSize) return `The ${fieldName} field size should be at least ${rules.minSize / 1000}KB.`;
             if (rules.validMimetypes && !rules.validMimetypes.includes(file.mimetype)) return `The ${fieldName} field mimetype should be ${rules.validMimetypes.join(" or ")}.`;
@@ -70,16 +77,16 @@ export default class FileValidator {
   }
 
   maxParts(count: number) {
-    this.rules.parts ?? this.rules.maxParts = count;
+    this.rules.maxParts = count;
     return this;
   }
 
   minParts(count: number) {
-    this.rules.parts ?? this.rules.minParts = count;
+    this.rules.minParts = count;
     return this;
   }
 
-  mimetypes(validMimetypes: string[]) {
+  mimetypes(...validMimetypes: string[]) {
     this.rules.validMimetypes = validMimetypes;
     return this;
   }
--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   testMatch: ["<rootDir>/tests/{feature,unit}/**/*.test.js"],
   testTimeout: 23000,
   setupFiles: [
-    './tests/connectDB.js',
+ //   './tests/connectDB.js',
     "./tests/registerHelpers.js",
   ],
-  globalTeardown: './tests/teardown.js',
+  //globalTeardown: './tests/teardown.js',
   modulePaths: [compilerOptions.baseUrl],
   preset: "ts-jest",
   testEnvironment: 'node',
