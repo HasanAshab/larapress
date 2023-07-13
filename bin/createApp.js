4aa247219dba48e72e50b4be3a1b81a73aa64e49
HasanAshab <samertask@gmail.com>
2023-07-13 18:06:26 +0600

    backup

--- bin/createApp.js
+++ bin/createApp.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log("hai")
+
+try {
+const { execSync } = require('child_process');
+//const path = require('path');
+
+//const ownPath = process.cwd();
+const folderName = process.argv[2];
+const repo = 'https://github.com/HasanAshab/larapress.git';
+
+console.log(`Downloading files from repo ${repo}`);
+execSync(`git clone --depth 1 ${repo} ${folderName}`);
+
+/*
+const artisanPath = path.join(ownPath, folderName, 'artisan.ts');
+console.log(artisanPath)
+execSync(`ts-node -r tsconfig-paths/register --transpile-only ${artisanPath} key:generate`);
+}*/
+catch (err) {
+  console.log(err)
+}
