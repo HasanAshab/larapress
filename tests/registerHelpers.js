4305095f3fb0f9ad58eadd018b859d283f80a125
HasanAshab <samertask@gmail.com>
2023-08-27 22:53:12 +0600

    backup

--- tests/registerHelpers.js
+++ tests/registerHelpers.js
@@ -5,7 +5,7 @@ global.fakeFile = (name) => {
   return `storage/test_files/${name}`;
 };
 
-const server = require("../main").default;
-console.log(require("../main"))
+const server = require("~/main").default;
+console.log(require("~/main"))
 console.log(server)
 global.request = require("supertest")(server);
