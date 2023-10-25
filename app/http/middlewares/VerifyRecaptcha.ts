d262a8b16b0465e6aae261acb220387c8f7c27d9
HasanAshab <samertask@gmail.com>
2023-10-25 21:59:50 +0600

    backup

--- app/http/middlewares/VerifyRecaptcha.ts
+++ app/http/middlewares/VerifyRecaptcha.ts
@@ -1,23 +1,24 @@
 import Middleware from "~/core/abstract/Middleware";
 import { Request, Response, NextFunction } from "express";
 import Config from "Config";
+import axios from "axios";
 
 export default class VerifyRecaptcha extends Middleware {
   async handle(req: Request, res: Response, next: NextFunction) {
-    const recaptchaResponse = req.body.recaptchaResponse;
-    const verificationResponse = await fetch('https://www.google.com/recaptcha/api/siteverify', {
-      method: 'POST',
+    const body = {
+      secret: Config.get("recaptcha.secretKey")
+      response: req.body.recaptchaResponse,
+    }
+    
+    const { success } = await axios.post('https://www.google.com/recaptcha/api/siteverify', body, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: `secret=${Config.get("recaptcha.secretKey")}&response=${recaptchaResponse}`,
     });
-    const verificationResult: any = await verificationResponse.json();
-    if (!verificationResult.success) {
-      return res.status(400).api({
-        message: 'reCAPTCHA verification failed!'
-      });
+    
+    if (success) {
+      return next();
     }
-    next();
+    res.status(400).message('reCAPTCHA verification failed!')
   }
 }
