3a1603cf54c0937a37af9afbc3f5259ee2a684af
HasanAshab <samertask@gmail.com>
2023-11-22 10:59:16 +0600

    backup

--- tests/unit/events/registered.test.ts
+++ tests/unit/events/registered.test.ts
@@ -15,15 +15,44 @@ describe("Registered Event", () => {
     user = await User.factory().unverified().create();
   });
 
-  it("should send verification email", async () => {
-    await new SendEmailVerificationNotification().dispatch({user});
+  it("should send verification email on internal method", async () => {
+    user.sendVerificationNotification = jest.fn();
+    
+    await new SendEmailVerificationNotification().dispatch({
+      user,
+      version: "v1",
+      method: "internal"
+    });
+    
+    expect(user.sendVerificationNotification).toHaveBeenCalledTimes(1);
+    expect(user.sendVerificationNotification).toHaveBeenCalledWith("v1"); 
     Notification.assertCount(1);
     Notification.assertSentTo(user, EmailVerificationNotification);
   });
   
+  it("shouldn't send verification email on social method", async () => {
+    user.sendVerificationNotification = jest.fn();
+    
+    await new SendEmailVerificationNotification().dispatch({
+      user,
+      version: "v1",
+      method: "social"
+    });
+    
+    expect(user.sendVerificationNotification).not.toBeCalled();
+    Notification.assertNothingSent();
+  });
+
+  
   it("should notify admins about new user", async () => {
     const admins = await User.factory().count(3).hasSettings().withRole("admin").create();
-    await new SendNewUserJoinedNotificationToAdmins().dispatch(user);
+    
+    await new SendNewUserJoinedNotificationToAdmins().dispatch({ 
+      user,
+      version: "v1",
+      method: "internal"
+    });
+    
     Notification.assertSentTo(admins, NewUserJoinedNotification);
   });
 });
